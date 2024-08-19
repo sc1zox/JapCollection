25169957e23f4d442669bda04494ff26180e4e67
scizox <scizox@localhost.localdomain>
2024-08-19 20:56:33 +0200

    cry

--- src/Fruehjar_24/Aufgabe3.java
+++ src/Fruehjar_24/Aufgabe3.java
@@ -3,7 +3,7 @@ package Fruehjar_24;
 public class Aufgabe3 {
     public static void main(String[] args) {
         int[] array = {2,4};
-        int[] result = komplettiere(array,40);
+        int[] result = komplettiere2(array,40);
 
         System.out.print("Komplettiere: [");
         for (int i = 0; i < result.length; i++) {
@@ -15,7 +15,7 @@ public class Aufgabe3 {
         System.out.print("]\n");
     }
 
-    static int[] komplettiere(int[] in, int max) {
+    static int[] komplettiere(int[] in, int max) { //cheating
         int[] counter = new int[50];
         counter[0] = in[0];
         counter[1] = in[1];
@@ -39,5 +39,27 @@ public class Aufgabe3 {
             ergebnis[j]=counter[j];
         }
     return ergebnis;
+    }
+
+    public static int[] komplettiere2(int[] in, int max){
+        int zahl1 = in[0];
+        int zahl2 = in[1];
+        int ergebnis = zahl1+zahl2;
+        int count = 3;
+
+        while(ergebnis+zahl2<=max){
+            zahl1=ergebnis;
+            ergebnis+=zahl2;
+            zahl2=zahl1;
+            count++;
+        }
+        int[] result = new int[count];
+        result[0]=in[0];
+        result[1]=in[1];
+
+        for (int i = 2; i < result.length; i++) {
+            result[i]=result[i-1]+result[i-2];
+        }
+        return result;
     }
 }
